722429294326bdcc4661e7cc08cf6cd8a8212eb5
Ryo Kato <katoryo55@gmail.com>
2015-07-26 00:35:57 +0900

    章立てを変えた

--- convert_scenario_into_denkinovel.rb
+++ convert_scenario_into_denkinovel.rb
@@ -13,12 +13,13 @@ class Scenario
   end
 
   def convert
-    @text.gsub!(/^;.*\n/, "")
-    @text.gsub!(/^\[.*\n/, "")
+    @text.gsub!(/^;.*(\r\n|\n)/, "")
+    @text.gsub!(/^\[.*(\r\n|\n)/, "")
     @text.gsub!(/^□.*\n/, "")
     @text.gsub!(/^【.*】/, "")
     @text.gsub!(/(\[.*?\])/, "")
     @text.gsub!(/;;＠(\n|\r\n)/, "\n[page]")
+    @text.gsub!(/(\r\n|\n){3,4}/, "\n[page]")
     @text.gsub!(/\r\n\r\n/, "")
     @text.gsub!(/\n\n/, "")
     @text.gsub!(/\{(.*?)\}/, '[\1]')
@@ -30,11 +31,14 @@ end
 
 
 (20..29).each do |n|
-  file_number = n 
+  file_number = n
   scenario = Scenario.new(File.read("stories/anathema_scenario#{file_number}.txt"), file_number)
   scenario.convert_and_output
 end
 
-file_number = 1
-scenario = Scenario.new(File.read("stories/anathema_scenario#{file_number}.txt"), file_number)
-scenario.convert_and_output
+
+(1..2).each do |n|
+  file_number = n
+  scenario = Scenario.new(File.read("stories/anathema_scenario#{file_number}.txt"), file_number)
+  scenario.convert_and_output
+end
